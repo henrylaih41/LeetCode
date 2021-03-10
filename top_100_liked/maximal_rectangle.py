5fbd2b3cee4a66ac909e0afbb8429ba00989be7c
henrylaih41 <henrylaih41@gmail.com>
2021-03-10 14:04:23 +0800

    3/10

--- top_100_liked/maximal_rectangle.py
+++ top_100_liked/maximal_rectangle.py
@@ -13,14 +13,14 @@ class Solution:
             ### O(c)
             maxx = max(maxx, self.largestRectangleArea(h))
         return maxx
-   
+  
     # O(len(heights)) = O(c)
     def largestRectangleArea(self, heights: List[int]) -> int:
         s = [(-1, -1)] # stack
         heights.append(-1) # dummy height
         maxx = 0
         for i, h in enumerate(heights):
-            pt = i 
+            pt = i
             while(h < s[-1][0]):
                     maxx = max(maxx, s[-1][0] * (i-s[-1][1]))
                     pt   = s[-1][1]
--- top_100_liked/unique_binary_search_trees.py
+++ top_100_liked/unique_binary_search_trees.py
@@ -1,17 +1,36 @@
 class Solution:
-    def numTrees(self, n: int) -> int:
-        if(n <= 1):
-            return 1
-        DP = [-1] * (n+1)
-        DP[0], DP[1] = 1, 1
-        return self.recur(n, DP)
-        
-    def recur(self, n, DP):
-        if(DP[n] != -1):
-            return DP[n]
-        summ = 0
-        for i in range(1, n+1):
-            summ += self.recur(i-1, DP) * self.recur(n-i, DP)
-        
-        DP[n] = summ
-        return summ
+    def numTrees(self, n: int) -> int:
+        if(n <= 1):
+            return 1
+        DP = [-1] * (n+1)
+        DP[0], DP[1] = 1, 1
+        return self.recur(n, DP)
+    def recur(self, n, DP):
+        if(DP[n] != -1):
+            return DP[n]
+        summ = 0
+        for i in range(1, n+1):
+            summ += self.recur(i-1, DP) * self.recur(n-i, DP)
+        DP[n] = summ
+        return summ
+
+    def validParenthess(self, n):
+        if(n <= 1):
+            return 1
+        DP = [-1] * (n+1)
+        DP[0], DP[1] = 1, 1
+        return self.util(n, DP)
+
+    def util(self, n, DP):
+        ### (0)*util(n-1) + ... + (n-1) * 0
+        if(DP[n] != -1):
+            return DP[n]
+        summ = 0
+        for i in range(0, n):
+            summ += self.util(i, DP) * self.util(n-i-1, DP)
+        DP[n] = summ
+        return summ
+
+s = Solution()
+for i in range(20):
+    print(s.validParenthess(i))
